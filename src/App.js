8ec232411e6ae2c2842d305ea304489f3db1c10e
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-20 21:30:19 -0600

    css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isMobileMenuOpen: false
+    }
+  }
+
+toggleMobileMenu = () => {
+  this.setState({isMobileMenuOpen: !this.state.isMobileMenuOpen})
 }
 
-export default App;
+
+
+
+render() {
+  return(
+
+<Fragment> 
+  <main>
+    <section className="section1">
+      <nav className="navbar">
+        <a href=".navbar">
+          <img className="logo" src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg">            
+          </img>
+        </a>
+        <div className="navbarRight">
+          <button className="menu" onClick={this.toggleMobileMenu}>MENU
+            <div className="bars">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </button>
+          <a className="link link1" href=".services">SERVICES</a>
+          <a className="link link2" href=".portfolio">PORTFOLIO</a>
+          <a className="link link3" href=".about">ABOUT</a>
+          <a className="link link4" href=".team">TEAM</a>
+          <a className="link link5" href=".contact">CONTACT</a>
+        </div>
+      </nav>
+      <div className="center">
+        <h2>Welcome To Our Studio!</h2>
+        <h1>IT'S NICE TO MEET YOU</h1>
+        <a href=".services">
+        <button className="button1" href=".services">TELL ME MORE</button>
+        </a>
+      </div>
+    </section>
+  </main>
+</Fragment>
+  )
+}
+
+}
+
