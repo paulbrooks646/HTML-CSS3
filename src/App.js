cf0873584240776d852d5b002f86db5a82e281b7
paulbrooks646 <paulbrooks646@gmail.com>
2020-05-25 15:52:46 -0600

    html/css3/finished

--- src/App.js
+++ src/App.js
@@ -25,10 +25,23 @@ render() {
     <section className="section1">
       <nav className="navbar">
         <a href=".navbar">
-          <img className="logo" src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg">            
+          <img className="logo" src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="Men's Grooming Supplies">            
           </img>
         </a>
         <div className="navbarRight">
+          <a className="link link1" href=".services">SERVICES</a>
+          <a className="link link2" href=".portfolio">PORTFOLIO</a>
+          <a className="link link3" href=".about">ABOUT</a>
+          <a className="link link4" href=".team">TEAM</a>
+          <a className="link link5" href=".contact">CONTACT</a>
+        </div>
+      </nav>
+      <nav className="mobile-navbar">
+        <a href=".navbar">
+          <img className="mobile-logo" src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="Men's Grooming Supplies">            
+          </img>
+        </a>
+        <div className="mobile-navbarRight">
           <button className="menu" onClick={this.toggleMobileMenu}>MENU
             <div className="bars">
               <div className="bar"></div>
@@ -36,11 +49,13 @@ render() {
               <div className="bar"></div>
             </div>
           </button>
-          <a className="link link1" href=".services">SERVICES</a>
-          <a className="link link2" href=".portfolio">PORTFOLIO</a>
-          <a className="link link3" href=".about">ABOUT</a>
-          <a className="link link4" href=".team">TEAM</a>
-          <a className="link link5" href=".contact">CONTACT</a>
+          <div className={`mobile-link-list ${this.state.isMobileMenuOpen ? "mobile-link-list-open" : null}`}>
+            <a className="mobile-link mobile-link1" href=".services">SERVICES</a>
+            <a className="mobile-link mobile-link2" href=".portfolio">PORTFOLIO</a>
+            <a className="mobile-link mobile-link3" href=".about">ABOUT</a>
+            <a className="mobile-link mobile-link4" href=".team">TEAM</a>
+            <a className="mobile-link mobile-link5" href=".contact">CONTACT</a>
+          </div>
         </div>
       </nav>
       <div className="center">
